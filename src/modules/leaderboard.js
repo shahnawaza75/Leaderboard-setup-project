49cdb571372a81180007dc7369df838f88bb6ca9
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-05-28 00:23:53 +0530

    Created files

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,4 +1,4 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fujWLO0ZaHh2nZQXugzx/scores/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FRZdMMyF1kjqYVxv98wr/scores/';
 const scoreTable = document.querySelector('.score-table');
 
 const addScore = async (userscore) => {
@@ -18,7 +18,7 @@ const getScore = async () => {
   const request = await fetch(url);
   const data = await request.json();
 
-  scoreTable.innerHTML = data.result
+  scoreTable.innerHTML = data.result.sort((a, b) => b.score - a.score)
     .map((score) => `<li>${score.user}: ${score.score}</li>`)
     .join('');
 };
